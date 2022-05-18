f3d0eacce6c5bfe7bbdb23812be75b921dcd4a71
elahe <elahekhodavrdi@gmail.com>
2022-05-18 07:30:32 -0700

    hichi

--- person.hpp
+++ person.hpp
@@ -1,11 +1,13 @@
 #ifndef __PERSON_HPP__
 #define __PERSON_HPP__
 #include <string>
+#include "define.hpp"
 class Person
 {
 public:
 protected:
-std::string name;
+    std::string name;
+    State state;
 };
 
 #endif
